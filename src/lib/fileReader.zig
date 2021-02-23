31dd7523dca2fe7fe3fc269ed9bdc935b7f3de8c
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2021-02-22 22:37:54 -0300

    refactor: simplify error handling in fileReader

--- src/lib/fileReader.zig
+++ src/lib/fileReader.zig
@@ -10,11 +10,7 @@ pub fn readArgument() Error![:0]u8 {
 
     _ = arguments.skip();
 
-    return arguments.next(allocator) orelse {
-        return Error.MissingArgument;
-    } catch |_| {
-        return Error.MissingArgument;
-    };
+    return arguments.next(allocator) orelse Error.MissingArgument catch Error.MissingArgument;
 }
 
 pub fn readAsUtf8(path: [:0]u8) Error![]u8 {
@@ -23,11 +19,7 @@ pub fn readAsUtf8(path: [:0]u8) Error![]u8 {
     };
     defer file.close();
 
-    const content = file.readToEndAlloc(std.heap.page_allocator, 4096) catch |_| {
-        return Error.FileTooBig;
-    };
-
-    return content;
+    return file.readToEndAlloc(std.heap.page_allocator, 4096) catch Error.FileTooBig;
 }
 
 pub fn readFile() Error![]u8 {
